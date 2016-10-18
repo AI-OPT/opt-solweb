8a99491ce8a6db1ad37212c87328fce660f9765f
jiaxiaoshuo <jiaxs@asiainfo.com>
2016-10-18 16:30:01 +0800

    简单页面初始化

---
+++ src/main/java/com/ai/opt/sol/web/controller/serviceDesign/ServiceDesignController.java
@@ -0,0 +1,31 @@
+package com.ai.opt.sol.web.controller.serviceDesign;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ai.opt.sol.web.base.model.ResponseData;
+
+@Controller
+@RequestMapping("/serviceDesign")
+public class ServiceDesignController {
+	
+	@RequestMapping("/add")
+	public ModelAndView addView(){
+		ModelAndView view = new ModelAndView("/serviceDesign/save");
+        return view;
+	}
+	
+	@RequestMapping("/save")
+	@ResponseBody
+	public ResponseData<String> save(HttpServletRequest request,HttpSession session){
+		//当prdlineId为空时  新增
+		
+		//当prdlineId非空时  修改
+		return new ResponseData<>(ResponseData.AJAX_STATUS_SUCCESS, "成功");
+	}
+}
